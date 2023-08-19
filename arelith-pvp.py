d9cdd87f59f1a2b29ac516b56932438c0fe9d0e2
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-08-19 13:15:25 +0100

    Update arelith-pvp.py

--- arelith-pvp.py
+++ arelith-pvp.py
@@ -1,12 +1,30 @@
-
-
+#Create Player 1
 hp = 0
-damage = 0
-spell_damage = 0
-dice_roll = 1d20
-AB = 0
 AC = 0
+AB = 0
+APR = 0,0,0,0
+damage = 0 #Melee Only
+spell_damage = 0 #Mage Only
 will_save = 0
 reflex_save = 0
 fortitude _save = 0
 str = 0
+dex = 0
+con = 0
+
+#Create Player 2
+hp = 0
+AC = 0
+AB = 0
+APR = 0,0,0,0
+damage = 0 #Melee Only
+spell_damage = 0 #Mage Only
+will_save = 0
+reflex_save = 0
+fortitude _save = 0
+str = 0
+dex = 0
+con = 0
+
+#Dice Roll
+dice_roll = 1d20
