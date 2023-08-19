12e2f26eeb18fe4b043fcfe571dd0f579fa17303
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-08-19 13:23:58 +0100

    Update arelith-pvp.py

--- arelith-pvp.py
+++ arelith-pvp.py
@@ -1,10 +1,12 @@
 #Create Player 1
-hp = 0
+class = fighter
+fighter_level = 30
+fighter_hp = CON * 
 AC = 0
 AB = 0
-APR = 0,0,0,0
-damage = 0 #Melee Only
-spell_damage = 0 #Mage Only
+APR = 4
+damage = 0 #Fighter Only
+spell_damage = 0 #Wizard Only
 will_save = 0
 reflex_save = 0
 fortitude _save = 0
@@ -13,12 +15,14 @@ dex = 0
 con = 0
 
 #Create Player 2
-hp = 0
+class = wizard
+wizard_hp = CON * 
+wizard_level = 30
 AC = 0
 AB = 0
-APR = 0,0,0,0
-damage = 0 #Melee Only
-spell_damage = 0 #Mage Only
+APR = 3
+damage = 0 #Fighter Only
+spell_damage = 0 #Wizard Only
 will_save = 0
 reflex_save = 0
 fortitude _save = 0
